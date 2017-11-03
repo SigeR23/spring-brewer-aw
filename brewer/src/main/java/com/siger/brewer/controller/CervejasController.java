b4a3ceaf9b74abe8ba4e39f18908027a1979f4e8
Regis Lessa <regis23.lf@gmail.com>
2017-11-03 20:35:16 -0200

    configurando o springmvc

---
+++ brewer/src/main/java/com/siger/brewer/controller/CervejasController.java
@@ -0,0 +1,14 @@
+package com.siger.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CervejasController {
+	
+	@RequestMapping("/cerveja/novo")
+	public String novo() {
+		return "cerveja/CadastroCerveja";
+	}
+	
+}
