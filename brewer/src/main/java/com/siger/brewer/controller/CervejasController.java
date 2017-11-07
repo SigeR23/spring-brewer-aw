c0641abdf68175fcaea124841c3ff5a3d5e74885
Regis Lessa <regis23.lf@gmail.com>
2017-11-07 20:00:28 -0200

    Começando com Spring (param + atributos) Forward/Redirect

--- brewer/src/main/java/com/siger/brewer/controller/CervejasController.java
+++ brewer/src/main/java/com/siger/brewer/controller/CervejasController.java
@@ -1,7 +1,15 @@
 package com.siger.brewer.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.siger.brewer.modal.Cerveja;
 
 @Controller
 public class CervejasController {
@@ -9,6 +17,21 @@ public class CervejasController {
 	@RequestMapping("/cerveja/novo")
 	public String novo() {
 		return "cerveja/CadastroCerveja";
+	}
+	
+	@RequestMapping(value = "/cerveja/novo", method = RequestMethod.POST)
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+		
+		if(result.hasErrors()) {
+			model.addAttribute("mensagem", "Erro no formulário");
+			return "cerveja/CadastroCerveja";
+		}
+		
+		attributes.addFlashAttribute("mensagem", "Cerveja Salva");
+		System.out.println("Param sku = " + cerveja.getSku());
+		System.out.println("Param nome = " + cerveja.getNome());
+		System.out.println("Cadastrar");
+		return "redirect:/cerveja/novo";
 	}
 	
 }
